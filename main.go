cbb6d9246a552b064706ccbe298d8025048b8d64
gagandeepahuja09 <gagandeepahuja09@gmail.com>
2025-12-21 23:16:19 +0530

    reading and writing in bytes: [length][payload][checksum]

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"bufio"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"hash/crc32"
+	"io"
 	"log"
 	"log/slog"
 	"os"
@@ -43,13 +46,73 @@ func (db *DB) cmdPut(args []string) error {
 }
 
 func (db *DB) writeToWal(key, value string) error {
-	if _, err := db.walFile.WriteString(fmt.Sprintf("PUT %s %s\n", key, value)); err != nil {
+	cmd := fmt.Sprintf("PUT %s %s\n", key, value)
+	buf := make([]byte, 4+len(cmd)+4)
+	checksum := crc32.ChecksumIEEE([]byte(cmd))
+	// 1. add length
+	binary.BigEndian.PutUint32(buf[0:4], uint32(len(cmd)))
+	// 2. add cmd / payload
+	copy(buf[4:4+len(cmd)], []byte(cmd))
+	// 3. add checksum
+	binary.BigEndian.PutUint32(buf[4+len(cmd):], checksum)
+	if _, err := db.walFile.Write(buf); err != nil {
 		slog.Error("WAL_WRITE_FAILED", map[string]interface{}{
 			"error": err.Error(),
 		})
 		return err
 	}
 	return db.walFile.Sync()
+}
+
+func readEntry(file *os.File) (payload []byte, err error) {
+	// 1. Read Length (4 bytes)
+	lengthBuf := make([]byte, 4)
+	_, err = io.ReadFull(file, lengthBuf)
+	if err == io.EOF {
+		return nil, io.EOF
+	}
+	if err == io.ErrUnexpectedEOF {
+		return nil, errors.New("partial write: incomplete length")
+	}
+	if err != nil {
+		return nil, err
+	}
+	// 2. Parse Length
+	payloadLength := binary.BigEndian.Uint32(lengthBuf)
+
+	// 3. Sanity Check
+	if payloadLength > 1_000_000 { // 1 MB max
+		return nil, errors.New("corrupt: length too large")
+	}
+
+	// 4. Read payload
+	payload = make([]byte, payloadLength)
+	_, err = io.ReadFull(file, payload)
+	if err == io.ErrUnexpectedEOF {
+		return nil, errors.New("partial write: incomplete payload")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// 5. Read checksum
+	checksumBuf := make([]byte, 4)
+	_, err = io.ReadFull(file, checksumBuf)
+	if err == io.ErrUnexpectedEOF {
+		return nil, errors.New("partial write: incomplete checksum")
+	}
+	if err != nil {
+		return nil, err
+	}
+
+	// 6. Verify checksum
+	storedChecksum := binary.BigEndian.Uint32(checksumBuf)
+	computedChecksum := crc32.ChecksumIEEE(payload)
+	if storedChecksum != computedChecksum {
+		return nil, errors.New("corrupt: checksum mismatch")
+	}
+
+	return payload, err
 }
 
 func buildDatabaseFromWal() (*DB, error) {
@@ -61,14 +124,23 @@ func buildDatabaseFromWal() (*DB, error) {
 		})
 		return nil, err
 	}
-	scanner := bufio.NewScanner(file)
-
 	db := &DB{
 		data:    data,
 		walFile: file,
 	}
-	for scanner.Scan() {
-		line := scanner.Text()
+
+	for {
+		payload, err := readEntry(file)
+		if err == io.EOF {
+			return db, nil
+		}
+		// for now, I will abort even in case of partial write
+		// todo: in case of partial write we should just truncate that log.
+		// we can also do that as part of listening to signal SIGTERM and SIGKILL?
+		if err != nil {
+			return nil, err
+		}
+		line := string(payload)
 		args := strings.Split(line, " ")
 		if len(args) != 3 {
 			return nil, errors.New("Expected exactly 2 arguments for PUT command\n")
@@ -77,7 +149,6 @@ func buildDatabaseFromWal() (*DB, error) {
 		value := args[2]
 		db.data[key] = value
 	}
-	return db, nil
 }
 
 func main() {
